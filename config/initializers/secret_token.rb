56c356fe120569b8c644f79ac393c0dce32e3b14
PallaviTS <pallavi046@gmail.com>
2014-04-05 16:06:59 +0530

    Youtube_API: Initial commit + basics fiel added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YoutubeOnRails::Application.config.secret_key_base = 'ec01d7ff48f2a82efb7afdc196f963c46ca97a49878e3bf310b64ad91742c301d0782fe49369647213f383e65c982c7c94f1873ee840710b04c645030204171f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YoutubeOnRails::Application.config.session_store :cookie_store, key: '_youtube_on_rails_session'
